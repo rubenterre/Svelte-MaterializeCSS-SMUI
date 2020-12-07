098e9d06eac435900c1de5464275aad8cd353628
Rubén Terré <rubenterre@users.noreply.github.com>
2020-12-07 19:19:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Svelte-Materialize-SMUI
+ Template of Svelte with Materialize CSS and Svelte Material UI
---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+</script>
+
+<main>
+  <div class="carousel">
+    <a class="carousel-item" href="#two!"><img src="https://via.placeholder.com/150" alt="1"></a>
+    <a class="carousel-item" href="#three!"><img src="https://via.placeholder.com/150" alt="2"></a>
+    <a class="carousel-item" href="#four!"><img src="https://via.placeholder.com/150" alt="3"></a>
+    <a class="carousel-item" href="#five!"><img src="https://via.placeholder.com/150" alt="4"></a>
+  </div>
+
+  <table class="striped">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Item Name</th>
+        <th>Item Price</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>Alvin</td>
+        <td>Eclair</td>
+        <td>$0.87</td>
+      </tr>
+      <tr>
+        <td>Alan</td>
+        <td>Jellybean</td>
+        <td>$3.76</td>
+      </tr>
+      <tr>
+        <td>Jonathan</td>
+        <td>Lollipop</td>
+        <td>$7.00</td>
+      </tr>
+    </tbody>
+  </table>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
